b544c94ff439610cd913a78abe894041846c049a
random25umezawa <bday7125@gmail.com>
2019-01-13 13:56:38 +0900

    初期状態。

---
+++ main.js
@@ -0,0 +1,22 @@
+const {app,BrowserWindow} = require("electron")
+let win;
+
+function createWindow(){
+	win = new BrowserWindow({width:600,height:480})
+	win.loadFile("index.html")
+	win.on("closed",_=>{
+		win = null
+	})
+}
+
+app.on("ready",createWindow)
+app.on("window-all-closed",_=>{
+	if(process.platform!=="darwin"){
+		app.quit()
+	}
+})
+app.on("active",_=>{
+	if(win===null){
+		createWindow()
+	}
+})
